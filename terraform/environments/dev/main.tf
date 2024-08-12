837dcead0dc28b1d6448122cf8bba8230dc33fa0
Manuel Fuentes <manuel.fuentes@curantissolutions.com>
2024-08-12 17:59:27 -0600

    update validations

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,5 +1,5 @@
 module "s3_bucket" {
   source      = "../../modules/s3"
-  bucket-name = "angular-website-terraform8"
+  bucket-name = "angular-website-terraform9"
   environment = "dev"
 }
--- terraform/environments/qa/main.tf
+++ terraform/environments/qa/main.tf
@@ -1,5 +1,5 @@
 module "s3_bucket" {
   source      = "../../modules/s3"
-  bucket-name = "angular-website-terraform8"
+  bucket-name = "angular-website-terrafor9"
   environment = "qa"
 }
