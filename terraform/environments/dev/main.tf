a16c1f1cbba6b2efc41fb9af1e00e1d4a9df3557
Manuel Fuentes <manuel.fuentes@curantissolutions.com>
2024-08-12 18:03:38 -0600

    update reference for url

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,5 +1,5 @@
 module "s3_bucket" {
   source      = "../../modules/s3"
-  bucket-name = "angular-website-terraform9"
+  bucket-name = "angular-website-terraform10"
   environment = "dev"
 }
--- terraform/environments/qa/main.tf
+++ terraform/environments/qa/main.tf
@@ -1,5 +1,5 @@
 module "s3_bucket" {
   source      = "../../modules/s3"
-  bucket-name = "angular-website-terrafor9"
+  bucket-name = "angular-website-terrafor10"
   environment = "qa"
 }
