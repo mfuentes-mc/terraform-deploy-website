c2fb6edd74f36b4f61cbe9b7868da8a0fe69e9f2
Manuel Fuentes <manuel.fuentes@curantissolutions.com>
2024-08-12 18:26:49 -0600

    update validations

--- terraform/modules/s3/variables.tf
+++ terraform/modules/s3/variables.tf
@@ -7,9 +7,3 @@ variable "environment" {
   description = "Environment variable"
   type        = string
 }
-
-variable "create_bucket" {
-  description = "Whether to create the S3 bucket"
-  type        = bool
-  default     = true
-}
